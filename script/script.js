5220a1fdea4723b1bd9be6eca8e9ff03ccff7350
peter munyambu <petermunyambu1000@gmail.com>
2025-06-01 20:16:35 +0300

    update style.css, index.html and script.js

---
+++ script/script.js
@@ -0,0 +1,6 @@
+<script>
+  document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault(); // prevent form from submitting
+    alert("Thank you! Your message has been submitted.");
+  });
+</script>
